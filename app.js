099493e1a96920c1911dfd1e6786aaf93f58df9f
Natalie Mall <nkmall@ucdavis.edu>
2020-07-16 14:56:00 -0700

    commented out some code to undo the error causing double-use of the port listener. NOT in a working state but error is fixed

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const http = require('http');
 let {requestListener} = require('./callbackFile.js');
-const PORT = process.env.PORT || 4000; //changed from 4001 to 4004 
+// const PORT = process.env.PORT || 4000; //changed from 4001 to 4004 
 
-var server = http.createServer(requestListener)
+// var server = http.createServer(requestListener)
 
-server.listen(PORT)
+// server.listen(PORT)
 
 const fs = require('fs')
 
 
 
 
-const fileStream = fs.createWriteStream('output.txt');  //should these go in this app.js file?
+// const fileStream = fs.createWriteStream('output.txt');  //should these go in this app.js file?
 
 
-fileStream.write('This is the first line!'); 
-fileStream.write('This is the second line!');
-fileStream.end();
+// fileStream.write('This is the first line!'); 
+// fileStream.write('This is the second line!');
+// fileStream.end();
 
 
-let events = require('events');
+// let events = require('events');
 
-let listenerCallback = (data) => {
-    console.log('Celebrate ' + data);
-}
+// let listenerCallback = (data) => {
+//     console.log('Celebrate ' + data);
+// }
 
-let myEmitter = new events.EventEmitter();
+// let myEmitter = new events.EventEmitter();
 
-myEmitter.on('celebration', listenerCallback);
-myEmitter.emit('celebration', 'birthday party')
+// myEmitter.on('celebration', listenerCallback);
+// myEmitter.emit('celebration', 'birthday party')
 
