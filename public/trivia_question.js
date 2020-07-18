9939541354599467a53c66b23adedaec8c415022
Natalie Mall <nkmall@ucdavis.edu>
2020-07-18 11:10:59 -0700

    working draft of trivia question display - does not yet include answers for selection

--- public/trivia_question.js
+++ public/trivia_question.js
@@ -4,7 +4,83 @@ console.log('hello from the trivia question client side')
 const displayElement = document.getElementById('current_question');
 const submit_button = document.getElementById('submit_button');
 
-displayElement.innerHTML = "Here is the question coming from the .js file"; 
+
+// fetch('/retrieve_question/', {
+//   method: 'GET',
+//   // body: JSON.stringify(jsonConst),
+//   // body: '{"mykey": "bigcat2"}',
+//   // body: bobcatStr,
+//   // headers: {
+//   // // 'Accept': 'application/json',
+//   // 'Content-Type': 'application/json'
+//   // 'Content-Type': 'text/plain'
+
+//   // 'Content-Type': 'application/x-www-form-urlencoded',
+//   // } 
+//   }).then(() => {
+//   // TODO something
+//   // refreshDisplay();  //add back in when ready to fetch a webpage saying "submission completed"
+//   console.log('test FETCH');
+//   // refreshDisplay();
+// });
+
+function refreshDisplay() {
+  fetch('/retrieve_question/') // gets /api/messages (GET is the default)
+    .then(result => result.json() // console.log(result) 
+      )
+    // .then(console.log(result))
+    .then(data => displayMessage(data) //console.log(data)
+      )
+    .catch(error => console.log('There was an error', error));
+}
+
+
+// fetch('/retrieve_question/') // gets /api/messages (GET is the default)
+//     .then(result => result.json() // console.log(result) 
+//       )
+//     // .then(console.log(result))
+//     .then(data => displayMessage(data) //console.log(data)
+//       )
+//     .catch(error => console.log('There was an error', error));
+
+
+function displayMessage(message1) {   
+
+  // var test5 = JSON.stringify(recent_quotes); // stringify = recipricol (parse)
+  // var test7 = JSON.parse(test5);
+
+  var message_parsed = message1[0];
+  var current_question = message_parsed['question'];
+  // console.log('test11', test11);
+
+  displayElement.innerHTML = current_question;    // clear old stuff in displayElement:
+
+  // var i;
+  //   for (i = 0; i < info; i++) {
+  //   // text += cars[i] + "<br>";
+  //   variable_name = test7[i];
+  //   console.log(variable_name);
+
+  //   displayElement.innerHTML += ( test7[i].name + '<br>' )
+  //   // .map(record => JSON.stringify(record))
+  //   // .join('<br>');
+  //   }
+}
+
+// displayElement.innerHTML = "Here is the question coming from the .js file"; 
+
+// const objValue = document.getElementById('happy_thought').value;
+
+const objValue = 'TEST4'
+const mykey2 = { "mykey" : objValue};
+console.log(objValue)
+console.log(mykey2)
+
+const bobcatStr = JSON.stringify(mykey2);
+console.log(bobcatStr);
+
+
+refreshDisplay();
 
 submit_button.addEventListener('click', () => {
     console.log('TEST2')
--- server.js
+++ server.js
@@ -1,28 +1,6 @@
 //trivia-crowd
 
 
-
-// const fs = require('fs');
-
-// module.exports = {
-//   requestListener: (req, res) => {
-//   fs.readFile('./testWebsite.html',  'utf-8', (err, data) => {
-//     if (err){
-//       res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(`${err}`);
-//     res.end();
-//     } else {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     res.end(); 
-//     }
-//   })
-// }
-// }
-
-
-
-
 const PORT = process.env.PORT || 5000;
 
 const express = require('express');
@@ -47,9 +25,9 @@ router.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get('/submitted_question/', (req, res, send) => {
-    res.send('Your question was submitted')
-    })
+// app.get('/submitted_question/', (req, res, send) => {
+//     res.send('Your question was submitted')
+//     })
 
 app.get('/db', async (req, res) => {     //creating the database if postgresQL
 try {
@@ -64,9 +42,9 @@ try {
 } 
 })
 
-app.get('/somewhere_else/', async (req, res) => {
-  console.log('Here we will place the confirmation page')
-})
+// app.get('/somewhere_else/', async (req, res) => {
+//   console.log('Here we will place the confirmation page')
+// })
 
 app.put('/table_update/', async (req, res) => {
 // await fakeNetworkDelay();
@@ -105,9 +83,99 @@ app.put('/table_update/', async (req, res) => {
 
       
     });
-app.get('/thanks/', async (req, res) => {
-  console.log('thank you for your submission')
+
+app.get('/retrieve_question/', async (req, res) => {
+
+  try{
+    const client = await pool.connect();
+
+    var question_id = 8;
+    let sql = 'SELECT (question) FROM  trivia_questions WHERE id = $1';
+    let params = [question_id];
+
+    const { rows: name } = await client.query(sql, params)
+      console.log('result:', name)
+    
+
+
+    let answers = 'SELECT (answera) FROM  trivia_questions WHERE id = $1';
+    let params2 = [question_id];
+
+    // client.query(answers, params2).then(res => {
+    // const result_answers = res.rows;
+    // // console.log('result 2:', res)
+    // })
+
+    // .finally(()=> client.end());
+
+    // let router = require('express').Router();
+    // let bodyParser = require('body-parser');
+    // router.use(bodyParser.json());
+    // console.log('res send thing', result_answers)
+    res.send(name);
+    client.release();
+  } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      } 
+// client.query('SELECT 1 + 4').then(res => {
+
+//     const result = R.head(R.values(R.head(res.rows)));
+
+//     console.log(result);
+// }).finally(() => client.end());
+
+
+
+  // console.log('current question:', current_question)
+  // res.send('question sent')
+
+  console.log('querry retrieval:', sql)
+  console.log('test retrieve question')
 })
+
+
+
+app.put('/retrieve_question/', async (req, res) => {
+// await fakeNetworkDelay();
+
+  try {
+    const client = await pool.connect();
+
+    //select the current 
+    const message = req.body.mykey; //"${message}"
+    console.log('message', message);
+
+//     // client.query(`INSERT INTO test_table(name) VALUES ( "${message}")`);
+//     // ^^commented out until the display can display all messages
+
+//     var id = 7;
+//     var name = message;
+//     let sql = 'INSERT INTO test_table (id, name) VALUES ($1, $2)';
+//     let params = [ id, name ];
+//     client.query(sql, params, function(err) {
+// // make sure you handle errors from here as well,
+// // including signaling `res` and `done`
+// }); 
+
+    res.send( "sent" );  // do we actually want to send anything?
+    client.release(); //changed from 'release' to 'end'
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  } 
+
+      
+    });
+
+
+
+
+
+
+// app.get('/thanks/', async (req, res) => {
+//   console.log('thank you for your submission')
+// })
 
 
 //port listening, which happens once at the end of the code 
